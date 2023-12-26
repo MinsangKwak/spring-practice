9b1a3c1b750f9412b5ed5109b21f1b482ca1cbda
minsangKwak <kmsdevwork@gmail.com>
2023-12-26 20:12:10 +0900

    [feat] application.properties, lombok

---
+++ src/main/java/com/sparta/springprepare/Memo.java
@@ -0,0 +1,25 @@
+package com.sparta.springprepare;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+//@AllArgsConstructor
+//@NoArgsConstructor
+@RequiredArgsConstructor
+public class Memo {
+    private final String username;
+    private String contents;
+}
+
+class Main {
+
+    public static void main(String[] args){
+        Memo memo = new Memo("Robbert");
+//        memo.setUsername("Robbie");
+        System.out.println(memo.getUsername());
+    }
+
+}
